0b1aae2dc7918fb61032815ce34c66456d3fb90c
Shivashankar Bhutekar <59565135+shivashankar16041990@users.noreply.github.com>
2023-02-18 15:19:57 +0530

    Delete main.dart

--- main.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:io';
-
-import 'package:dart_frog/dart_frog.dart';
-
-Future<HttpServer> run(Handler handler, InternetAddress ip, int port) {
-
-  return serve(handler, ip, port,poweredByHeader: null);
-}
